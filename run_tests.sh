319b0f349a6397647a1fb89aced2c2fccfd45fde
Uwe Winter <uwwint@gmail.com>
2026-01-30 13:00:27 +1100

    fix: do not parallelize selenium tests because they fire up a galaxy instance.

--- run_tests.sh
+++ run_tests.sh
@@ -398,6 +398,8 @@ do
           GALAXY_TEST_TOOL_CONF="lib/galaxy/config/sample/tool_conf.xml.sample,test/functional/tools/sample_tool_conf.xml"
           report_file="./run_selenium_tests.html"
           skip_client_build=""
+          # Selenium tests start Galaxy + DB; running in parallel can race database creation.
+          parallel_args=()
           if [ $# -gt 1 ]; then
               selenium_script=$2
               shift 2
